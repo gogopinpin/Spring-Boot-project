757ab3fc21538d188bf4cc25c83730298c398837
Tuntun pig <134057222+gogopinpin@users.noreply.github.com>
2023-09-02 21:08:35 +0800

    Add files via upload

---
+++ src/test/java/com/Ray/springbootmall/SpringbootMallApplication.java
@@ -0,0 +1,14 @@
+package com.Ray.springbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallApplication {
+
+	public static void main(String[] args) {
+// 		使用 SpringApplication 的 run 方法啟動 Spring Boot 應用程式
+		SpringApplication.run(SpringbootMallApplication.class, args);
+	}
+
+}
