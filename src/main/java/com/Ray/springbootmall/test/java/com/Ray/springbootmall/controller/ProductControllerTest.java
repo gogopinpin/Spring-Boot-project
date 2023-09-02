e3e4b8fb982148bde8cada0a5643ba7284d294e6
Tuntun pig <134057222+gogopinpin@users.noreply.github.com>
2023-09-02 20:51:44 +0800

    Add files via upload

---
+++ src/main/java/com/Ray/springbootmall/test/java/com/Ray/springbootmall/controller/ProductControllerTest.java
@@ -0,0 +1,265 @@
+package com.Ray.springbootmall.controller;
+
+import com.Ray.springbootmall.dto.ProductRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.Ray.springbootmall.constant.ProductCategory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+//      測試查詢商品成功
+    public void getProduct_success() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/products/{productId}", 1);
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print()) //運行的時候在下面的 console 印出這個 api 的返回結果
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.productName", equalTo("蘋果（澳洲）")))
+                .andExpect(jsonPath("$.category", equalTo("FOOD")))
+                .andExpect(jsonPath("$.imageUrl", notNullValue()))
+                .andExpect(jsonPath("$.price", notNullValue()))
+                .andExpect(jsonPath("$.stock", notNullValue()))
+                .andExpect(jsonPath("$.description", notNullValue()))
+                .andExpect(jsonPath("$.createdDate", notNullValue()))
+                .andExpect(jsonPath("$.lastModifiedDate", notNullValue()));
+    }
+
+    public void getProduct_notFound() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/products/{productId}", 20000);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(404));
+    }
+
+//      測試創建商品成功
+    @Transactional //回復測試前
+    @Test
+    public void createProduct_success() throws Exception {
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.setProductName("test food product");
+        productRequest.setCategory(ProductCategory.FOOD);
+        productRequest.setImageUrl("http://test.com");
+        productRequest.setPrice(100);
+        productRequest.setStock(2);
+
+        String json = objectMapper.writeValueAsString(productRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(201))
+                .andExpect(jsonPath("$.productName", equalTo("test food product")))
+                .andExpect(jsonPath("$.category", equalTo("FOOD")))
+                .andExpect(jsonPath("$.imageUrl", equalTo("http://test.com")))
+                .andExpect(jsonPath("$.price", equalTo(100)))
+                .andExpect(jsonPath("$.stock", equalTo(2)))
+                .andExpect(jsonPath("$.description", nullValue()))
+                .andExpect(jsonPath("$.createdDate", notNullValue()))
+                .andExpect(jsonPath("$.lastModifiedDate", notNullValue()));
+    }
+
+    @Transactional
+    @Test
+    public void createProduct_illegalArgument() throws Exception {
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.setProductName("test food product");
+
+        String json = objectMapper.writeValueAsString(productRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+    }
+
+//      測試更新商品成功
+    @Transactional
+    @Test
+    public void updateProduct_success() throws Exception {
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.setProductName("test food product");
+        productRequest.setCategory(ProductCategory.FOOD);
+        productRequest.setImageUrl("http://test.com");
+        productRequest.setPrice(100);
+        productRequest.setStock(2);
+
+        String json = objectMapper.writeValueAsString(productRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put("/products/{productId}", 3)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.productName", equalTo("test food product")))
+                .andExpect(jsonPath("$.category", equalTo("FOOD")))
+                .andExpect(jsonPath("$.imageUrl", equalTo("http://test.com")))
+                .andExpect(jsonPath("$.price", equalTo(100)))
+                .andExpect(jsonPath("$.stock", equalTo(2)))
+                .andExpect(jsonPath("$.description", nullValue()))
+                .andExpect(jsonPath("$.createdDate", notNullValue()))
+                .andExpect(jsonPath("$.lastModifiedDate", notNullValue()));
+    }
+
+    @Transactional
+    @Test
+    public void updateProduct_illegalArgument() throws Exception {
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.setProductName("test food product");
+
+        String json = objectMapper.writeValueAsString(productRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put("/products/{productId}", 3)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+
+    }
+
+    @Transactional
+    @Test
+    public void updateProduct_productNotFound() throws Exception {
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.setProductName("test food product");
+        productRequest.setCategory(ProductCategory.FOOD);
+        productRequest.setImageUrl("http://test.com");
+        productRequest.setPrice(100);
+        productRequest.setStock(2);
+
+        String json = objectMapper.writeValueAsString(productRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put("/products/{productId}", 20000)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(404));
+    }
+
+//      測試刪除商品成功
+    @Transactional
+    @Test
+    public void deleteProduct_success() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .delete("/products/{productId}", 5);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(204));
+    }
+
+    @Transactional
+    @Test
+    public void deleteProduct_deleteNonExistingProduct() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .delete("/products/{productId}", 20000);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(204));
+    }
+
+//      測試查詢商品列表
+    @Test
+    public void getProducts() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/products");
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.limit", notNullValue()))
+                .andExpect(jsonPath("$.offset", notNullValue()))
+                .andExpect(jsonPath("$.total", notNullValue()))
+                .andExpect(jsonPath("$.results", hasSize(5)));//results是List可以用 hasSize(預期返回幾筆數據)
+    }
+
+    @Test
+    public void getProducts_filtering() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/products")
+                .param("search", "B")
+                .param("category", "CAR");
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.limit", notNullValue()))
+                .andExpect(jsonPath("$.offset", notNullValue()))
+                .andExpect(jsonPath("$.total", notNullValue()))
+                .andExpect(jsonPath("$.results", hasSize(2)));
+        //預期當查詢商品的關鍵字是 B且分類是CAR 時只會去找到 2 筆的商品數據
+    }
+
+    @Test
+    public void getProducts_sorting() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/products")
+                .param("orderBy", "price")
+                .param("sort", "desc");
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.limit", notNullValue()))
+                .andExpect(jsonPath("$.offset", notNullValue()))
+                .andExpect(jsonPath("$.total", notNullValue()))
+                .andExpect(jsonPath("$.results", hasSize(5)))
+                .andExpect(jsonPath("$.results[0].productId", equalTo(6)))
+                .andExpect(jsonPath("$.results[1].productId", equalTo(5)))
+                .andExpect(jsonPath("$.results[2].productId", equalTo(7)))
+                .andExpect(jsonPath("$.results[3].productId", equalTo(4)))
+                .andExpect(jsonPath("$.results[4].productId", equalTo(2)));
+        //也可以用List 一筆一筆的去取得 results 裡面的商品數據
+        //在 results 的第一個商品數據他的 product id 的值是否為 6
+    }
+
+    @Test
+    public void getProducts_pagination() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/products")
+                .param("limit", "2")
+                .param("offset", "2");
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.limit", notNullValue()))
+                .andExpect(jsonPath("$.offset", notNullValue()))
+                .andExpect(jsonPath("$.total", notNullValue()))
+                .andExpect(jsonPath("$.results", hasSize(2)))
+                .andExpect(jsonPath("$.results[0].productId", equalTo(5)))
+                .andExpect(jsonPath("$.results[1].productId", equalTo(4)));
+    }
+}
+
---
+++ src/main/java/com/Ray/springbootmall/test/java/com/Ray/springbootmall/controller/UserControllerTest.java
@@ -0,0 +1,210 @@
+package com.Ray.springbootmall.controller;
+
+import com.Ray.springbootmall.dto.UserLoginRequest;
+import com.Ray.springbootmall.dto.UserRegisterRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.Ray.springbootmall.dao.UserDao;
+import com.Ray.springbootmall.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserDao userDao;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+//      測試註冊新帳號成功
+    @Test
+    public void register_success() throws Exception {
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("test1@gmail.com");
+        userRegisterRequest.setPassword("123");
+
+        String json = objectMapper.writeValueAsString(userRegisterRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(201))
+                .andExpect(jsonPath("$.userId", notNullValue()))
+                .andExpect(jsonPath("$.email", equalTo("test1@gmail.com")))
+                .andExpect(jsonPath("$.createdDate", notNullValue()))
+                .andExpect(jsonPath("$.lastModifiedDate", notNullValue()));
+
+//      檢查資料庫中的密碼不為明碼
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+        assertNotEquals(userRegisterRequest.getPassword(), user.getPassword());
+    }
+
+//      測試email格式
+    @Test
+    public void register_invalidEmailFormat() throws Exception {
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("dsfsdfsfsdf");
+        userRegisterRequest.setPassword("123");
+
+        String json = objectMapper.writeValueAsString(userRegisterRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+
+    }
+
+//      測試註冊新帳號時的重複 email
+    @Test
+    public void register_emailAlreadyExist() throws Exception {
+//      先註冊一個帳號
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("test2@gmail.com");
+        userRegisterRequest.setPassword("123");
+
+        String json = objectMapper.writeValueAsString(userRegisterRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(201));
+
+//      再次註冊同樣的email
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+
+    }
+
+//      登入
+    public void login_success() throws Exception {
+//      先註冊新帳號
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("test3@gmail.com");
+        userRegisterRequest.setPassword("123");
+
+        register(userRegisterRequest);
+
+//      再測試登入功能
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+        userLoginRequest.setEmail(userRegisterRequest.getEmail());
+        userLoginRequest.setPassword(userRegisterRequest.getPassword());
+
+        String json = objectMapper.writeValueAsString(userRegisterRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.userId", notNullValue()))
+                .andExpect(jsonPath("$.email", equalTo(userRegisterRequest.getEmail())))
+                .andExpect(jsonPath("$.createdDate", notNullValue()))
+                .andExpect(jsonPath("$.lastModifiedDate", notNullValue()));
+
+    }
+
+//      密碼錯誤
+    @Test
+    public void login_wrongPassword() throws Exception {
+//      先註冊新帳號
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("test4@gmail.com");
+        userRegisterRequest.setPassword("123");
+
+        register(userRegisterRequest);
+
+//      測試密碼輸入錯誤的情況
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+        userLoginRequest.setEmail(userRegisterRequest.getEmail());
+        userLoginRequest.setPassword("unknown");
+
+        String json = objectMapper.writeValueAsString(userLoginRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+    }
+
+//      email 格式錯誤
+    @Test
+    public void login_invalidEmailFormat() throws Exception {
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+        userLoginRequest.setEmail("hkbudsr324");
+        userLoginRequest.setPassword("123");
+
+        String json = objectMapper.writeValueAsString(userLoginRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+    }
+
+//      email 不存在
+    @Test
+    public void login_emailNotExist() throws Exception {
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+        userLoginRequest.setEmail("unknown@gmail.com");
+        userLoginRequest.setPassword("123");
+
+        String json = objectMapper.writeValueAsString(userLoginRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+    }
+
+
+//      測試註冊功能的輔助方法，簡化測試碼的重複性
+    private void register(UserRegisterRequest userRegisterRequest) throws Exception {
+        String json = objectMapper.writeValueAsString(userRegisterRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(201));
+    }
+}
