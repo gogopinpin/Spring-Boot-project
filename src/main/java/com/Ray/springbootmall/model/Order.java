e3f4442152e66c6ff7564199db224e8112095c0d
Tuntun pig <134057222+gogopinpin@users.noreply.github.com>
2023-09-02 20:34:05 +0800

    Add files via upload

---
+++ src/main/java/com/Ray/springbootmall/model/Order.java
@@ -0,0 +1,64 @@
+package com.Ray.springbootmall.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+
+    private Integer orderId;
+    private Integer userId;
+    private Integer totalAmount;
+    private Date createdDate;
+    private Date lastModifiedDate;
+
+//    一個訂單可能包含多個訂單項目，每個項目代表訂單中的一個商品
+    private List<OrderItem> orderItemList;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Integer totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public List<OrderItem> getOrderItemList() {
+        return orderItemList;
+    }
+
+    public void setOrderItemList(List<OrderItem> orderItemList) {
+        this.orderItemList = orderItemList;
+    }
+}
---
+++ src/main/java/com/Ray/springbootmall/model/User.java
@@ -0,0 +1,59 @@
+package com.Ray.springbootmall.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
+
+public class User {
+
+    private Integer userId;
+    private String email;
+
+//    隱藏密碼屬性在 JSON 序列化時的顯示，以增加使用者資料的安全性
+    @JsonIgnore
+    private String password;
+
+    private Date createdDate;
+    private Date lastModifiedDate;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createDate) {
+        this.createdDate = createDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
